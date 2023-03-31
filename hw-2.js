06bce8e6111cf229ca3660564995bd9720b8432c
denniztr <denniztr@gmail.com>
2023-03-31 13:35:21 +0300

    new js hw2 hw3 files

---
+++ hw-2.js
@@ -0,0 +1,54 @@
+// let a = 10;
+// let a = 20;
+// alert(a);
+
+// let yearOfIssueIphone = 'Год выпуска первого Iphone 2007';
+// alert(yearOfIssueIphone);
+
+// let founderOfJavaScript = 'Имя создателя JavaScript Брендан Эйх';
+// alert(founderOfJavaScript);
+
+// let a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(b / a);
+
+// let a = 2;
+// let result = 2 ** 5;
+// alert(result);
+
+// let a = 9;
+// let b = 2; 
+// alert(a % b);
+
+// let num = 1;
+// // num +=5;
+// // num -=3;
+// // num *=7;
+// // num /=3;
+// // num +=1;
+// // num -=1;
+// alert(num);
+
+// let age = prompt('How old are you?')
+// alert(age);
+
+// const user = {
+//     name: 'Dennis',
+//     age: 25,
+//     isAdmin: false
+// }
+
+// user['City Of Residence'] = 'Izmir';
+// user.age = 25;
+// delete user['City Of Residence'];
+
+// console.log(user);
+
+// let info = prompt('Какую информацию хотите узнать о пользователе?', 'name', 'age', 'isAdmin');
+// alert(user[info]);
+
+// let name = prompt('what s ur name?');
+// alert(`hello, ${name}!`);
